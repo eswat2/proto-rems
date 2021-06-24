e194d32fd15e0e1aa2300a5f5471ee27b2c1eef0
Richard Hess <eswat2@gmail.com>
2021-06-24 10:34:47 -0700

    add snapshots of the new markdown output

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const base = {
   px: 2,
 }
 
+const debug = false
+const markdown = true
 const skip = []
 
 /**
@@ -13,24 +15,38 @@ const skip = []
  * @param {int} start
  * @return {object}
  */
-const getSizes = (stop = 900, start = 0) => {
+const getSizes = (stop = 100, start = 0, halves = false) => {
   // The following generates an array of increasing values from the totalSizes above.
-  const sizeArray = Array.from(Array((stop * 2) + 1).keys())
-  const sliced = sizeArray.slice((start * 2), sizeArray.length)
-  console.log('-- sliced: ', sliced.length, sliced)
+  const sizeArray = Array.from(Array(stop * (halves ? 2 : 1) + 1).keys())
+  const sliced = sizeArray.slice(start * (halves ? 2 : 1), sizeArray.length)
+  debug && console.log("-- sliced: ", sliced.length, sliced)
   const sizes = []
 
   sliced.forEach((x) => {
     const { tag, rem, px } = base
-    const size = x * tag
-    const rems = x * rem
-    const pixels = x * px
+    const size = x * tag * (halves ? 1 : 2)
+    const rems = x * rem * (halves ? 1 : 2)
+    const pixels = x * px * (halves ? 1 : 2)
     if (!skip.includes(size)) {
       sizes.push({ size, rems, pixels })
     }
   })
 
-  console.table(sizes, ['size', 'rems', 'pixels'])
+  //
+  if (markdown) {
+    console.log("# Tailwind spacing scale:")
+    console.log('')
+    console.log(`range: ${start}-${stop} ${ halves ? 'including half scale...' : ''}`)
+    console.log('')
+    console.log("| size | rems | pixels |")
+    console.log("| :--- | :--- | ---: |")
+
+    sizes.forEach(({ size, rems, pixels }) => {
+      console.log(`| ${size} | ${rems} | ${pixels} |`)
+    })
+  } else {
+    console.table(sizes, ["size", "rems", "pixels"])
+  }
 }
 
-getSizes(80, 70)
+getSizes()
